611e5e2871efb72f3ccedc11989988c68c10f2e4
Asmare Zelalem <asmarestu12@gmail.com>
2026-09-17 11:21:51 +0300

    change api address to deployed api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const DEFAULT_API_BASE_URL = 'http://localhost:8080';
+const DEFAULT_API_BASE_URL = 'https://api.render.com/deploy/srv-dalop8ek1f9s738vquo0?key=1YItEqlRkCs';
 
 import { clearTokens, getAccessToken, getRefreshToken, setStoredUser, setTokens } from '@/lib/auth-storage';
 
